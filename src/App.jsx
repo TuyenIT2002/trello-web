38268ce62bd87e6fe53f5aef074bf7fead234efa
TuyenIT2002 <tuyenlinh15081405@gmail.com>
2024-08-26 23:26:00 +0700

    dark light p1

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,26 @@ import { Button } from '@mui/material';
 import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
 import Typography from '@mui/material/Typography';
 
+import {useColorScheme} from '@mui/material/styles';
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light');
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  );
+}
+
+
 function App() {
   return (
     <>
+    <ModeToggle/>
     <div>tuyen dev</div>
     <Typography variant="body2" color="text.secondary">Heading</Typography>
      <Button variant="contained">Hello world</Button>
