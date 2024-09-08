3f1e1734643ddd2e3c0bd68af3f6748088c54bf0
TuyenIT2002 <tuyenlinh15081405@gmail.com>
2024-09-08 23:50:34 +0700

    refactor_project_structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,11 @@
 import './App.css'
-import Container  from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box'
-import {useColorScheme} from '@mui/material/styles';
-
-
+import Board from './pages/Boards/_id'
 
 function App() {
   return (
-    <Container disableGutters maxWidth={false} sx={{height:'100vh',backgroundColor:'primary.main'}}>
-    <Box sx={{
-      backgroundColor:'primary.light',
-      width:'100%',
-      height:(theme)=>theme.trello.appBarHeight,
-      display:'flex',
-      alignItems:'center'
-    }}>
-      Header
-    </Box>
-    <Box sx={{
-      backgroundColor:'primary.dark',
-      width:'100%',
-      height:(theme)=>theme.trello.boardBarHeight,
-      display:'flex',
-      alignItems:'center'
-    }}>
-      Board Bar
-    </Box>
-    <Box sx={{
-      backgroundColor:'primary.main',
-      width:'100%',
-      height:(theme)=>`calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
-      display:'flex',
-      alignItems:'center'
-    }}>
-      Board Content
-    </Box>
-    </Container>
+    <>
+      <Board />
+    </>
   )
 }
 
