935beba1c76b4abccac74e296dd866de2dc76db6
TuyenIT2002 <tuyenlinh15081405@gmail.com>
2024-10-17 22:40:56 +0700

    drag_drop_column

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,11 +1,38 @@
+import {useState, useEffect} from 'react'
 import Box from '@mui/material/Box';
-import ListColumns from './ListColumns/ListColumns'
-import {mapOrder} from '~/untils/sorts'
+import ListColumns from './ListColumns/ListColumns';
+import {mapOrder} from '~/untils/sorts';
+import {DndContext ,MouseSensor, TouchSensor, useSensor, useSensors} from '@dnd-kit/core';
+import { arrayMove } from '@dnd-kit/sortable';
 
+/* eslint-disable react/prop-types*/
 function BoardContent({board}) {
-    const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+
+    // const pointerSensor = useSensor(PointerSensor,{activationConstraint: {distance: 10}})
+    const mouseSensor = useSensor(MouseSensor,{activationConstraint: {distance: 10}})
+    const touchSensor = useSensor(TouchSensor,{activationConstraint: {delay: 250, tolerance:5}})
+
+    const sensors = useSensors(mouseSensor, touchSensor)
+    const [orderedColumns, setOederedColumns] = useState([])
+
+    useEffect(()=>{
+        setOederedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+    },[board])
+
+    const handleDragEnd =(event)=>{
+        console.log('handleDragEnd :', event);
+        const {active, over} = event
+        if (!over) return
+        if(active.id !== over.id){
+            const oldIndex = orderedColumns.findIndex(c => c._id === active.id)
+            const newIndex = orderedColumns.findIndex(c => c._id === over.id)
+            const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+            setOederedColumns(dndOrderedColumns)
+        }
+    }
+
     return ( 
-        <>
+        <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
            <Box sx={{
             backgroundColor: (theme)=>theme.palette.mode == 'dark' ? '#34495e' : '#0984e3',
             width:'100%',
@@ -18,7 +45,7 @@ function BoardContent({board}) {
                 {/* Nếu không có hàm sắp xếp thì sẽ là <ListColumns columns= {board?.columns}/ */}
                 <ListColumns columns= {orderedColumns}/> 
             </Box>
-        </>
+        </DndContext>
      );
 }
 
